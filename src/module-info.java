7f2ce143eea82ef0f0d5f0061f34566313c68bc5
ThanDucc <89775012+ThanDucc@users.noreply.github.com>
2021-12-07 13:26:25 +0700

    Update

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,4 @@
-module bomb {
+module bomberman {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
@@ -14,4 +14,15 @@ module bomb {
 
     exports uet.oop.bomberman.ControllerClasses;
     opens uet.oop.bomberman.ControllerClasses to javafx.fxml, javafx.graphics;
+    exports uet.oop.bomberman.entities;
+    opens uet.oop.bomberman.entities to javafx.fxml, javafx.graphics;
+    exports uet.oop.bomberman.entities.monster;
+    opens uet.oop.bomberman.entities.monster to javafx.fxml, javafx.graphics;
+    exports uet.oop.bomberman.entities.staticEntities;
+    opens uet.oop.bomberman.entities.staticEntities to javafx.fxml, javafx.graphics;
+    exports uet.oop.bomberman.entities.item;
+    opens uet.oop.bomberman.entities.item to javafx.fxml, javafx.graphics;
+    exports uet.oop.bomberman.entities.dynamicEntities;
+    opens uet.oop.bomberman.entities.dynamicEntities to javafx.fxml, javafx.graphics;
+
 }
